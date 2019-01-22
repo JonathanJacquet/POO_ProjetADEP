3e8c5297e2b889b2ad7a63643008935e405f5386
aziza <aaziza.pollet@gmail.com>
2019-01-22 22:18:40 +0100

    atgoriesManagement.php finished

--- controller/categoryController.php
+++ controller/categoryController.php
@@ -1,43 +1,43 @@
 <?php
 
-class categoryController
-{
+class categoryController{
   public function showListCategories() {
-  $categoriesManagement = new categoriesManagement();
-  $categories = $categoriesManagement->getCategories();
+    $categoriesManagement = new categoriesManagement();
+    $categories = $categoriesManagement->getCategories();
     require "view/listCategoriesView.php";
   }
 
   public function showAddCategories(){
-    $categoriesManagement = new categoriesManagement();
-    $category = new category($_POST);
-    $categoriesManagement->addCategory($category)
-    redirectTo("categoriesList");
+    if(!empty($_POST) && isset($_POST["addCategory"])) {
+      $catgoriesManager = new catgoriesManager();
+      $category= new catgory($_POST);
+      if($categoriesManager->addCategory($category))
+      redirectTo("categoriesList");
 
-  }
+    }
 
-  public function showUpdateCategories()
-  $categoriesManagement = new categoriesManagement();
+    public function showUpdateCategories(){
+      $id = htmlspecialchars($_GET["id"]);
+      $categoriesManager = new categoriesManagement();
+      $category = $categoriesManagement->getCategory($id);
+      if(!empty($_POST)) {
+        if ($categoriesManager->updateCategories($_POST)) {
+          redirectTo("categoriesList");
+        }
+      }
+      require "view/modifyCategorieView.php";
+    }
 
-  {
-    $id = htmlspecialchars($_GET["id"]);
-    $category = getCategory($_GET["id"]);
-    if(!empty($_POST)) {
-      if (updateCategories($_POST, $id)) {
+
+    public function showDeleteCategories(){
+      if(isset($_GET["id"])) {
+        $id = htmlspecialchars($_GET("id"));
+        $categoriesManager = new categoriesManagement();
+        if($categoriesManagement->deleteCategory($id))
+      }
         redirectTo("categoriesList");
       }
+
     }
-    require "view/modifyCategorieView.php";
-  }
 
-
-  public function showDeleteCategories()
-  $categoriesManagement = new categoriesManagement();
-
-  {
-    if(deleteCategories($_GET["id"])) {
-      redirectTo("categoriesList");
-    }
-  }
-
-  ?>
+    ?>
