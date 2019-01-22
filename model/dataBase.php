0b365e169d0e440f2e85dec860cf4267bc978f8e
Jo <jonathan.jacquet62@laposte.net>
2019-01-22 14:33:19 +0100

    create function entity user

--- model/dataBase.php
+++ model/dataBase.php
@@ -4,9 +4,9 @@
  */
 class dataBase {
   const host  = "";
-  const dbName = "";
-  const login = "";
-  const mdp = "";
+  const dbName = "signing";
+  const login = "phpmyadmin";
+  const mdp = "O7dwjfb9";
   static public function BD() {
     $db = new PDO("mysql:host=" . self::host .";dbname=" . self::dbName , self::login, self::mdp);
     return $db;
--- model/entity/user.php
+++ model/entity/user.php
@@ -6,13 +6,87 @@
 class user extends entity
 {
   protected $status;
+  protected $name;
+  protected $firstname;
+  protected $mail;
+  protected $street;
+  protected $city;
+  protected $pc;
+  protected $npe;
+  protected $sex;
 
+//setter list
   public function setStatus($status) {
     $this->status = $status;
   }
 
+  public function setName($name) {
+    $this->status = $tratus;
+  }
+
+  public function setFirstname($firstname) {
+    $this->firstname = $firstname;
+  }
+
+  public function setMail($mail) {
+    $this->mail = $mail;
+  }
+
+  public function setStreet($street) {
+    $this->street = $street;
+  }
+
+  public function setCity($city) {
+    $this->city = $city;
+  }
+
+  public function setPc($pc) {
+    $this->pc = $pc;
+  }
+
+  public function setNpe($npe) {
+    $this->npe = $npe;
+  }
+
+  public function setSex($sex) {
+    $this->sex = $sex;
+  }
+
+//getter list
   public function getStatus() {
     return $this->status;
+  }
+
+  public function getName() {
+    return $this->name;
+  }
+
+  public function getFirstname() {
+    return $this->firstname;
+  }
+
+  public function getMail() {
+    return $this->mail;
+  }
+
+  public function getStreet() {
+    return $this->street;
+  }
+
+  public function getCity() {
+    return $this->city;
+  }
+
+  public function getPc() {
+    return $this->pc;
+  }
+
+  public function getNpe() {
+    return $this->npe;
+  }
+
+  public function getSex() {
+    return $this->sex;
   }
 
   function __construct()
