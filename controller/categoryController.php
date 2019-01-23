c40775e878a961754c13b3e0b57ada634ccfe564
aziza <aaziza.pollet@gmail.com>
2019-01-23 12:12:57 +0100

    to pull before push

--- controller/categoryController.php
+++ controller/categoryController.php
@@ -1,12 +1,15 @@
 <?php
 
 class categoryController{
+
+  //get all gategories
   public function showListCategories() {
     $categoriesManagement = new categoriesManagement();
     $categories = $categoriesManagement->getCategories();
     require "view/listCategoriesView.php";
   }
 
+  //add gategory
   public function showAddCategories(){
     if(!empty($_POST) && isset($_POST["addCategory"])) {
       $catgoriesManager = new catgoriesManager();
@@ -15,29 +18,33 @@ class categoryController{
       redirectTo("categoriesList");
 
     }
+  }
 
-    public function showUpdateCategories(){
-      $id = htmlspecialchars($_GET["id"]);
-      $categoriesManager = new categoriesManagement();
-      $category = $categoriesManagement->getCategory($id);
-      if(!empty($_POST)) {
-        if ($categoriesManager->updateCategories($_POST)) {
-          redirectTo("categoriesList");
-        }
-      }
-      require "view/modifyCategorieView.php";
-    }
-
-
-    public function showDeleteCategories(){
-      if(isset($_GET["id"])) {
-        $id = htmlspecialchars($_GET("id"));
-        $categoriesManager = new categoriesManagement();
-        if($categoriesManagement->deleteCategory($id))
-      }
+  //update category
+  public function showUpdateCategories(){
+    $id = htmlspecialchars($_GET["id"]);
+    $categoriesManager = new categoriesManagement();
+    $category = $categoriesManagement->getCategory($id);
+    if(!empty($_POST)) {
+      if ($categoriesManager->updateCategories($_POST)) {
         redirectTo("categoriesList");
+
+        require "view/modifyCategorieView.php";
       }
-
     }
+  }
 
-    ?>
+
+  //delete gategory
+  public function showDeleteCategories(){
+    if(isset($_GET["id"])) {
+      $id = htmlspecialchars($_GET("id"));
+      $categoriesManager = new categoriesManagement();
+      if($categoriesManagement->deleteCategory($id))
+      redirectTo("categoriesList");
+    }
+  }
+
+}
+
+?>
--- model/categoriesManagement.php
+++ model/categoriesManagement.php
@@ -1,14 +1,14 @@
 <?php
 class categoriesManagement extends manager{
   public function getCategorises(){
-    $query = $this->getDb()$query->('SELECT * FROM categories');
-    $data = $query->fetchAll(PDO::FETCH_CLASS, "categorie");
+    $query = $this->getDb()('SELECT * FROM categories');
+    $rusult = $query->fetchAll(PDO::FETCH_CLASS, "categorie");
     $query->closeCursor();
-    return $data;
+    return new catgory($result);
   }
 
-public function getCategory($id){
-    $query = $this->getDb()prepare->('SELECT * FROM catégories WHERE id = ? ');
+  public function getCategory($id){
+    $query = $this->getDb()('SELECT * FROM catégories WHERE id = ? ');
     $query->excute([$id]);
     $query->fetchAll(PDO::FETCH_CLASS, 'categorie');
     $categorie = $query->fetch();
@@ -21,7 +21,7 @@ public function getCategory($id){
 
 
   public function addCategory(category $category) {
-    $query = $this->getDb()->prepare("INSERT INTO categories(gategory_name) VALUES(:category_name)");
+    $query = $this->getDb()("INSERT INTO categories(gategory_name) VALUES(:category_name)");
     $result = $query->execute([
       "category_name" => $compte->getName()
     ]);
@@ -31,7 +31,7 @@ public function getCategory($id){
 
 
   function deleteCategory($id) {
-    $request = $this->getDb()->prepare("DELETE FROM categories WHERE id = ?");
+    $request = $this->getDb()("DELETE FROM categories WHERE id = ?");
     $request->execute([$id]);
 
     $request->closeCursor();
@@ -40,7 +40,7 @@ public function getCategory($id){
 
 
   function updateCategory(category $category) {
-    $request = $this->getDb()->prepare("UPDATE categories SET category_name WHERE id = ?");
+    $request = $this->getDb()("UPDATE categories SET category_name WHERE id = ?");
     $request->execute([$category->getName(), $category->getName()]);
 
     $request->closeCursor();
@@ -49,5 +49,4 @@ public function getCategory($id){
   }
 
 }
-}
- ?>
+?>
