ec33378faf99def52a192a4b543dcd007f1ad37f
Jo <jonathan.jacquet62@laposte.net>
2019-01-25 16:15:31 +0100

    list users + sort users ( ne marche pas)

--- model/dataBase.php
+++ model/dataBase.php
@@ -5,8 +5,8 @@
 class dataBase {
   const host  = "localhost";
   const dbName = "signing";
-  const login = "phpmyadmin1";
-  const mdp = "Azerty59";
+  const login = "phpmyadmin";
+  const mdp = "O7dwjfb9";
 
   static public function BD() {
     $db = new PDO("mysql:host=" . self::host .";dbname=" . self::dbName , self::login, self::mdp);
